7833f3ec59d98715c4d38b71054973745c8cc976
LiCai <1981824361@qq.com>
2024-11-05 13:45:25 +0800

    feat: electron中使用puppeteer

--- src-electron/main-process/electron-main.js
+++ src-electron/main-process/electron-main.js
@@ -1,22 +1,41 @@
-import { app, BrowserWindow, nativeTheme } from 'electron'
+import { app, BrowserWindow, ipcMain, nativeTheme } from "electron";
+import puppeteer from "puppeteer-core";
+import pie from "puppeteer-in-electron";
 
-try {
-  if (process.platform === 'win32' && nativeTheme.shouldUseDarkColors === true) {
-    require('fs').unlinkSync(require('path').join(app.getPath('userData'), 'DevTools Extensions'))
-  }
-} catch (_) { }
+let mainWindow;
 
-/**
- * Set `__statics` path to static files in production;
- * The reason we are setting it here is that the path needs to be evaluated at runtime
- */
-if (process.env.PROD) {
-  global.__statics = __dirname
+let global = {
+  browser: null,
+  page: null,
+};
+const openPmos = async (event, params) => {
+  _setLog(params);
+  const window = new BrowserWindow();
+  const url = params.url;
+  await window.loadURL(url);
+  // window.maximize();
+  // window.show();
+  const page = await pie.getPage(global.browser, window);
+  global.page = page;
+  // 等待页面加载完成
+  await page.waitForSelector('input[name="username"]');
+  // 输入用户名
+  await page.type('input[name="username"]', params.username);
+  await page.waitForTimeout(1000);
+  // 执行脚本
+  _setLog(page.url());
+};
+
+function _setLog(msg) {
+  if (!mainWindow) return;
+  mainWindow.webContents.send("setLog", msg);
 }
 
-let mainWindow
+function registerApiHandler() {
+  ipcMain.handle("openPmos", openPmos);
+}
 
-function createWindow () {
+async function createWindow() {
   /**
    * Initial window options
    */
@@ -32,26 +51,55 @@ function createWindow () {
 
       // More info: /quasar-cli/developing-electron-apps/electron-preload-script
       // preload: path.resolve(__dirname, 'electron-preload.js')
-    }
-  })
+    },
+  });
 
-  mainWindow.loadURL(process.env.APP_URL)
+  mainWindow.loadURL(process.env.APP_URL);
 
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+
+  mainWindow.webContents.openDevTools();
 }
 
-app.on('ready', createWindow)
+async function main() {
+  try {
+    if (
+      process.platform === "win32" &&
+      nativeTheme.shouldUseDarkColors === true
+    ) {
+      require("fs").unlinkSync(
+        require("path").join(app.getPath("userData"), "DevTools Extensions")
+      );
+    }
+  } catch (_) {}
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+  /**
+   * Set `__statics` path to static files in production;
+   * The reason we are setting it here is that the path needs to be evaluated at runtime
+   */
+  if (process.env.PROD) {
+    global.__statics = __dirname;
   }
-})
+  await pie.initialize(app);
+  await app.whenReady();
+  global.browser = await pie.connect(app, puppeteer);
 
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
+  createWindow();
+  registerApiHandler();
+  app.on("ready", () => {});
+
+  app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+      app.quit();
+    }
+  });
+
+  app.on("activate", () => {
+    if (mainWindow === null) {
+    }
+  });
+}
+
+main();
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,15 +1,51 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <div class="container">
+    <ul>
+      <li
+        v-for="(item, index) in data"
+        :key="index"
+        @click.stop="() => _handleClick(item)"
+      >
+        {{ item.name }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import { ipcRenderer } from "electron";
+
 export default {
-  name: 'PageIndex'
-}
+  name: "PageIndex",
+  mounted() {
+    ipcRenderer.on("setLog", this.logFn);
+  },
+  data() {
+    return {
+      data: [
+        {
+          name: "知乎",
+          url: "https://www.zhihu.com/",
+          username: "18280689542",
+        },
+      ],
+    };
+  },
+  methods: {
+    logFn(e, args) {
+      console.info(args);
+    },
+    _handleClick(item) {
+      console.log("_handleClick", item);
+
+      ipcRenderer.invoke("openPmos", item);
+    },
+  },
+};
 </script>
+<style lang="scss">
+.container {
+  height: 100%;
+  width: 100%;
+}
+</style>
